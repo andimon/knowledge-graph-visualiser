cc4e62cefd5d156740c76e888f329eb14234d3ae
andimon <andrevella2001@gmail.com>
2024-03-25 20:36:31 +0100

    fix knowledge asset feature

--- create_knowledge_graph.js
+++ create_knowledge_graph.js
@@ -6,18 +6,32 @@ function getFragmentFromIRI(iri) {
   return iri.split('#').pop();
 }
 
-function get_knowledge_asset_features(triples) {
-  let features = triples.filter(t => t.object == "KnowledgeAssetFeature" && t.predicate == "subClassOf").map(t => ({ type: 'KnowledgeAssetFeature', name: t.subject }));
-  return features;
+function get_knowledge_asset_features_values(triples) {
+  let feature_values = [];
+  let vals = [];
+  let features = triples.filter(t => t.object == "KnowledgeAssetFeature" && t.predicate == "subClassOf").map(t=>t.subject);
+  for(let f of features){
+    vals = triples.filter(t => t.object == f && t.predicate == 'subClassOf' && t.subject.includes(f+'Value')).map(t => t.subject.replace(f+'Value',''));
+    vals = vals.map(v => ({ type: 'KnowledgeAssetFeature', name: v}))
+    feature_values.push(...vals)
+  }
+  return feature_values;
 }
 
+
+function get_feature_value(triples,feature,feature_assignment){
+  let value = triples.find(t => t.subject == feature_assignment && t.predicate == "type" && t.object.includes(feature+'Value')).object;
+  value = value.replace(feature+'Value','');
+  return value;
+}
 
 
 function get_knowledge_asset_features_relationships(triples) {
   let features = triples.filter(t => t.object == "KnowledgeAssetFeature" && t.predicate == "subClassOf").map(t => t.subject);
   let feature_relationships = [];
+  let feature_value = "";
   for (let feature of features) {
-    feature_relationships.push(...triples.filter(t => t.predicate == "has" + feature).map(t => ({ source: t.subject, target: feature, type: t.predicate })));
+    feature_relationships.push(...triples.filter(t => t.predicate == "has" + feature).map(t => ({ source: t.subject, target: get_feature_value(triples,feature,t.object), type: t.predicate })));
   }
   return feature_relationships
 }
@@ -49,7 +63,7 @@ function getEdges(triples) {
 function getNodes(triples) {
   const knowledgeAssetNodes = triples.filter(t => (t.predicate == 'type' && t.object == 'KnowledgeAsset')).map(t => ({ type: 'KnowledgeAsset', name: t.subject }));
   const personNodes = triples.filter(t => (t.predicate == 'type' && t.object == 'Person')).map(t => ({ type: 'Person', name: t.subject }));
-  return [].concat.apply([], [knowledgeAssetNodes, personNodes, get_knowledge_asset_features(triples)]);
+  return [].concat.apply([], [knowledgeAssetNodes, personNodes, get_knowledge_asset_features_values(triples)]);
 }
 
 /*
